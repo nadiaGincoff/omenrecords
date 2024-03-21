9fdd8d94dd87e30fd285c56057ad281fa87546ef
nadia <nadia.gincoff@gmail.com>
2024-03-21 18:25:17 +0100

    clg

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -6,6 +6,8 @@ import ArtworkWithName from "./_components/artworkWithName";
 export default async function Home() {
   const {releases, results} = await getReleases();
 
+  console.log(releases, results, "get releases ===============");
+
   return (
     <section className="mt-20 flex min-h-screen w-full flex-col items-center justify-start md:gap-32">
       <SubtitleHeader subtitle="Music" />
